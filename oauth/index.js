451c12e12541977d590015105cc9d83e36d16872
Paco Navarro <packarbell@gmail.com>
2018-11-25 21:03:04 +0100

    disaster

--- oauth/index.js
+++ oauth/index.js
@@ -19,7 +19,7 @@ function redirect () {
       success: function (result) {
         console.log(result);
         if (result.generatedAccessToken) {
-          window.location.href = (decodeURI(params.redirect_uri) +
+          window.location.href = (params.redirect_uri +
             `#access_token=${result.generatedAccessToken}&token_type=bearer&state=${params.state}`);
         }
       },
